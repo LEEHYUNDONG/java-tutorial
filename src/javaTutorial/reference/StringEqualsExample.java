624b948d070dfebaa5760c5b97d4dd2fac0a1a0f
LEEHYUNDONG <l.steve10@yahoo.com>
2021-09-27 18:00:53 +0900

    참조 타입

---
+++ src/javaTutorial/reference/StringEqualsExample.java
@@ -0,0 +1,34 @@
+package javaTutorial.reference;
+
+public class StringEqualsExample {
+    public static void main(String[] args) {
+        String strVar1 = "이현동";
+        String strVar2 = "이현동";
+
+        if (strVar1 == strVar2) {
+            System.out.println("참조가 같음");
+        }else{
+            System.out.println("참조가 다름");
+        }
+        if(strVar1.equals(strVar2)) {
+            System.out.println("1, 2 문자열이 같습니다.");
+        }
+        strVar1 = null;
+        System.out.println(strVar2);
+        
+        String name1 = new String("helloe");
+        String name2 = new String("helloe");
+        if (name2 == name2) {
+            System.out.println("참조가 같음");
+        }else{
+            System.out.println("참조가 다름");
+        }
+        if(name1.equals(name2)) {
+            System.out.println("1, 2 문자열이 같습니다.");
+        }
+        name1 = null;
+        System.out.println(name2);
+        System.out.println(name1);
+
+    }
+}
--- src/javaTutorial/reference/dataType.java
+++ src/javaTutorial/reference/dataType.java
@@ -3,7 +3,12 @@ package javaTutorial.reference;
 public class dataType {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("123world");
-
+        String name1 = "신용권";
+        String name2 = "신용권";
+        if (name1 == name2) {
+            System.out.println("Same");
+        }else{
+            System.out.println("Different");
+        }
     }
 }
