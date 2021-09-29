e991f04114472b2ee9942a793df139a8bbf77bb1
LEEHYUNDONG <l.steve10@yahoo.com>
2021-09-29 16:29:17 +0900

    carEx

---
+++ src/javaTutorial/Class/Car.java
@@ -0,0 +1,12 @@
+package javaTutorial.Class;
+
+public class Car {
+    String company = "Hyundai";
+    String model = "Gv80";
+    String color = "Black";
+    int maxSpeed = 350;
+    int speed;
+    Car (){
+        this.speed = 300;
+    }
+}
