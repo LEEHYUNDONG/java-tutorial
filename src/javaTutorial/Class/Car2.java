828d56a7e380ba3dd3a273883d6a16752835d3f0
LEEHYUNDONG <l.steve10@yahoo.com>
2021-09-29 17:00:59 +0900

    car2Ex

---
+++ src/javaTutorial/Class/Car2.java
@@ -0,0 +1,25 @@
+package javaTutorial.Class;
+
+public class Car2 {
+    String company = "Hyundai";
+    String model;
+    String color;
+    int maxSpeed;
+    int speed;
+
+    Car2 (){
+
+    }
+    Car2(String model){
+        this.model = model;
+    }
+    Car2(String model, String color){
+        this.model = model;
+        this.color = color;
+    }
+    Car2(String model, String color, int maxSpeed){
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+}
