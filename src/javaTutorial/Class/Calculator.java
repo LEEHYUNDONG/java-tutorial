3da5891198ce3f19cdd329c0fa8aac538df194fa
LEEHYUNDONG <l.steve10@yahoo.com>
2021-09-29 17:09:36 +0900

    calculator example

---
+++ src/javaTutorial/Class/Calculator.java
@@ -0,0 +1,11 @@
+package javaTutorial.Class;
+
+public class Calculator {
+
+    void powerOn(){
+        System.out.println("Power On.");
+    }
+    double divide(double n1, double n2){
+        return n1/n2;
+    }
+}
---
+++ src/javaTutorial/Class/calculatorEx.java
@@ -0,0 +1,9 @@
+package javaTutorial.Class;
+
+public class calculatorEx {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.powerOn();
+        System.out.println("res :"+c.divide(10.3, 20.4));
+    }
+}
