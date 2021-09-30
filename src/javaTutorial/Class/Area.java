3f43b20c1189a923a2bc6f897899674b1ca14662
LEEHYUNDONG <l.steve10@yahoo.com>
2021-09-30 20:41:34 +0900

    method overloading

---
+++ src/javaTutorial/Class/Area.java
@@ -0,0 +1,10 @@
+package javaTutorial.Class;
+
+public class Area {
+    double areaRectangle(double w){
+        return w*w;
+    }
+    double areaRectangle(double w, double h){
+        return w*h;
+    }
+}
---
+++ src/javaTutorial/Class/areaEx.java
@@ -0,0 +1,10 @@
+package javaTutorial.Class;
+
+public class areaEx {
+    public static void main(String[] args) {
+        Area a = new Area();
+
+        System.out.println(a.areaRectangle(10));
+        System.out.println(a.areaRectangle(10,20));
+    }
+}
